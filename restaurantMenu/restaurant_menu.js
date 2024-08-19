3fbda1821f8b3ce9b761efb20cf44744c7140edd
Mobeen-developer <rock.of.mob@gmail.om>
2024-08-19 12:40:45 -0400

    added price to food items

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak -$5', 'Pasta -$66', 'Burger -$33', 'Salmon -$90'];
+const dessertMenu = ['Cake -$7', 'Ice Cream -87', 'Pudding -$98', 'Fruit Salad -$65'];
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
         let mainCourseItem = '';
